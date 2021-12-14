fe665f24f1975ef05809fab5ccbfe83d1fdc8789
ameliafadhillah57@gmail.com <[ameliafadhillah57@gmail.com]>
2021-12-14 23:11:07 +0700

    variable and data type in javascript

---
+++ problem345.js
@@ -0,0 +1,14 @@
+//problem 3
+let name="Amel";
+console.log(`Hello ${name}! Saya Java Script, bahasa yang sangat menyenangkan `);
+//problem 4
+let alas= 20;
+let tinggi= 25;
+let luas= (1/2)*alas*tinggi;
+console.log(luas);
+//problem 5
+let t=20;
+let r=4;
+const pi=3.14;
+let luas_permukaan=2*pi*r*(r+t);
+console.log(luas_permukaan);
